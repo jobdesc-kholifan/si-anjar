1de2751d35e3da3b7b00bc886bea449c71cf3b02
kholifanalfon <kholifanalfon99@gmail.com>
2021-12-19 10:12:16 +0700

    Commit 1.9-3 - change link document preview

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -109,6 +109,6 @@ if (!function_exists('fileUnlink')) {
 if (!function_exists('DBImage')) {
 
     function DBImage($alias = 'preview') {
-        return DB::raw(sprintf("CONCAT('%s/',REPLACE(directory, '/', '_'), '/%s/view/', file_name) as %s", url('preview'), env('APP_KEY_VALUE'), $alias));
+        return DB::raw(sprintf("CONCAT('%s/',REPLACE(directory, '/', '_'), '/%s/response/', file_name) as %s", url('preview'), env('APP_KEY_VALUE'), $alias));
     }
 }
